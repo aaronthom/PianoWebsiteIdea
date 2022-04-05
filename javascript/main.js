65c0df93998095dba260d40656af3f0390ecec98
aaronthomclear <hampeaaron@gmail.com>
2022-04-05 22:21:12 +0200

    finished desktop

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-    const hamburgerMenu = document.querySelector('.menuBtn');
-    const navMenu = document.querySelector('.nav__List');
 
-    hamburgerMenu.addEventListener('click', () => {
-        hamburgerMenu.classList.toggle('active');
-        navMenu.classList.toggle('active');
-    })
+const hamburgerMenu = document.querySelector('.menuBtn');
+const navMenu = document.querySelector('.nav__List');
 
-    document.querySelectorAll('.listItem').forEach(n => n.addEventListener('click', () => {
-        hamburgerMenu.classList.remove('active');
-        navMenu.classList.remove('active');
-    }))
-    document.querySelectorAll('.listItemContact').forEach(n => n.addEventListener('click', () => {
-        hamburgerMenu.classList.remove('active');
-        navMenu.classList.remove('active');
-    }))
-}
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.listItem').forEach(n => n.addEventListener('click', () => {
+    hamburgerMenu.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
+document.querySelectorAll('.listItemContact').forEach(n => n.addEventListener('click', () => {
+    hamburgerMenu.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
+
